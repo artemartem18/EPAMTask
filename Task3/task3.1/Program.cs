aad2537ea44b7d76846ee6bb1f265ce531721f50
artemartem18 <55949264+artemartem18@users.noreply.github.com>
2020-03-22 22:24:52 +0400

    Add files via upload

---
+++ Task3/task3.1/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Задание 3. Строки
+ Дана строка, вывести только те слова, которые встречаются в ней только один раз.
+ */
+using System;
+
+namespace task3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stroka = "Как сделать так Как чтобы сделать вывести только те слова, те которые встречаются в которые тексте только один раз";
+            string[] separators = { ",", ".", "!", "?", ";", ":", " " };
+            var words = stroka.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            Task3(words);
+            static void Task3(string []words)
+            {
+                for (int i = 0; i < words.Length; i++)
+                {
+                    int n = 0;
+                    for (int j = 0; j < words.Length; j++)
+                    {
+                        if (i == j) continue;
+                        if (words[i] == words[j]) n++;
+                    }
+                    if (n == 0)
+                    {
+                        Console.WriteLine(words[i]);
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+
+
+
+
+
+
+    }
+    
+}
+           
+            
+            
+           
